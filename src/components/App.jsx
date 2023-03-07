31f406011bebdf62e141dddfc2558aeb07632a5a
qudlaty <106681377+Chriss350@users.noreply.github.com>
2023-03-07 23:53:40 +0100

    upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Contacts } from './Contacts/Contacts';
+import { Navbar } from './Navbar/Navbar';
+import 'react-notifications/lib/notifications.css';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    name: '',
+    number: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+    const contact = { id: nanoid(), name, number };
+    contacts.some(el => el.name === name)
+      ? alert(`${name} is already on the contacts list`)
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
+  };
+
+  getContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
+    );
+  };
+
+  searchContacts = e => {
+    e.preventDefault();
+
+    const value = e.target.value.toLowerCase();
+    this.setState({ filter: value });
+  };
+
+  removeContact = id => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar search={this.searchContacts} />
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <ContactForm onSubmit={this.addContact} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-6 mx-auto mt-5">
+              <Contacts
+                contactsArr={this.getContacts()}
+                remove={this.removeContact}
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
